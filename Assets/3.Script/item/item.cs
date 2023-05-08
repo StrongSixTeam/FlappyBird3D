77dbe8c4cac573d1d2cb80678285e36fd9c17a2a
Dani Kang <90600217+kdi2344@users.noreply.github.com>
2023-05-08 10:58:01 +0900

    1.2.0
    REMOVE 쓸데없는 코드

--- Assets/3.Script/item/item.cs
+++ Assets/3.Script/item/item.cs
@@ -4,22 +4,14 @@ using UnityEngine;
 
 public class item : MonoBehaviour
 {
-    private float rotationSpeed = 50f; //회전 속도
     public int type = 1; //결정해주기 1: 커지는, 2: 점수, 3: 캐릭터 변경
 
     void Update()
     {
-        //항상 빙글 돌기
-        //transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
         if (transform.position.x < -5)
         {
             gameObject.SetActive(false);
         }
-    }
-
-    public void activeFalse() //플레이어 스크립트에서 item과 부딪힐 때 아이템 끄기 불러오기
-    {
-        gameObject.SetActive(false);
     }
 
     public void waitSeconds()
@@ -27,8 +19,4 @@ public class item : MonoBehaviour
         gameObject.SetActive(false);
     }
 
-    public void setActive()
-    {
-        transform.position = new Vector3(-10, transform.position.y, transform.position.z);
-    }
 }
